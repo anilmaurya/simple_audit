7ba05d75b733c9792647e8572abbba613913e4e3
Anil Maurya <anil@joshsoftware.com>
2014-07-16 15:20:02 +0530

    fix filename of generated migration file

--- generators/simple_audit_migration/simple_audit_migration_generator.rb
+++ generators/simple_audit_migration/simple_audit_migration_generator.rb
@@ -2,7 +2,7 @@ class SimpleAuditMigrationGenerator < Rails::Generator::Base
 
   def manifest
     record do |m|
-      m.migration_template 'migration.rb', "db/migrate", :migration_file_name => 'simple_audit_migration'
+      m.migration_template 'migration.rb', "db/migrate", :migration_file_name => 'simple_audit_migration.rb'
     end
   end
   
